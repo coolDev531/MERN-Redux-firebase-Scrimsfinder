da3e5f9b6d86fe4ef86ad00c7840ab80776d9b25
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-21 13:07:28 -0400

    update Scrims.jsx,  create scrims.mock.js

---
+++ client/src/mocks/scrims.mock.js
@@ -0,0 +1,24 @@
+export const mockScrims = [
+  {
+    teamOne: [],
+    teamTwo: [],
+    casters: [],
+    gameStartTime: '2021-08-21T15:30:25.877Z',
+    lobbyHost: null,
+    _id: '61211c3e9c3fdf22f409cd58',
+    createdAt: '2021-08-21T15:31:10.334Z',
+    updatedAt: '2021-08-21T15:31:10.334Z',
+    __v: 0,
+  },
+  {
+    teamOne: [],
+    teamTwo: [],
+    casters: [],
+    gameStartTime: '2021-08-21T15:45:11.000Z',
+    lobbyHost: null,
+    _id: '61211d629c3fdf22f409cd5a',
+    createdAt: '2021-08-21T15:36:02.293Z',
+    updatedAt: '2021-08-21T15:36:02.293Z',
+    __v: 0,
+  },
+];
--- client/src/screens/Scrims.jsx
+++ client/src/screens/Scrims.jsx
@@ -1,14 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { CurrentUserContext } from '../context/currentUser';
+import { mockScrims } from '../mocks/scrims.mock';
 
 export default function Scrims() {
   const [currentUser] = useContext(CurrentUserContext);
-  console.log({ currentUser });
+  const [scrims, setScrims] = useState([]);
+
+  useEffect(() => {
+    setScrims(mockScrims);
+  }, []);
+
   return (
     <div>
       <h1>home</h1>
       <h2>welcome:</h2>
       {currentUser && <pre>{JSON.stringify(currentUser, null, 2)}</pre>}
+
+      <div id="scrims-container">
+        {scrims.map((scrim) => (
+          <div class="one-scrim-container">
+            <h1>scrim</h1>
+            <h2>
+              Game Start: {new Date(scrim.gameStartTime).toLocaleString()}
+            </h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
