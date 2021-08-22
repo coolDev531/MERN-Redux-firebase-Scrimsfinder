8880f6bb520d2e09ce85ded758bd06be895cde87
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-22 19:54:06 -0400

    create models/user.js

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// not currently used.
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = User = mongoose.model('users', UserSchema);
