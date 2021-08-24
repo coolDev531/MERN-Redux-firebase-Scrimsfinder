f88ee410614159c3993549d3adb39478b29da92d
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-23 23:41:34 -0400

    create introForms.jsx

---
+++ client/src/components/IntroForms.jsx
@@ -0,0 +1,99 @@
+import { TextField } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+
+export default function IntroForms({
+  handleChange,
+  currentFormIndex,
+  userData,
+  setUserData,
+  rankData,
+  setRankData,
+  divisionsWithNumbers,
+}) {
+  const nameForm = (
+    <Grid item container justify="space-evenly">
+      <TextField
+        type="text"
+        name="name"
+        value={userData.name}
+        onChange={(e) => handleChange(e, setUserData)}
+        onKeyPress={(e) => {
+          // if user enters special characters don't do anything
+          if (!/^\w+$/.test(e.key)) e.preventDefault();
+        }}
+        placeholder="summoner name"
+        required
+      />
+      <TextField
+        type="text"
+        name="discord"
+        value={userData.discord}
+        onChange={(e) => handleChange(e, setUserData)}
+        placeholder="Discord (name and #)"
+        required
+      />
+    </Grid>
+  );
+
+  const divisionForm = (
+    <>
+      <label htmlFor="rankDivision">Rank</label>
+      <select
+        name="rankDivision"
+        required
+        value={rankData.rankDivision}
+        onChange={(e) => handleChange(e, setRankData)}>
+        {[
+          'Unranked',
+          'Iron',
+          'Bronze',
+          'Silver',
+          'Gold',
+          'Platinum',
+          'Diamond',
+          'Master',
+          'Grandmaster',
+          'Challenger',
+        ].map((value) => (
+          <option value={value}>{value}</option>
+        ))}
+      </select>
+
+      {/* exclude this number select from divisions without numbers */}
+      {divisionsWithNumbers.includes(rankData.rankDivision) && (
+        <select
+          name="rankNumber"
+          required={divisionsWithNumbers.includes(rankData.rankDivision)}
+          value={rankData.rankNumber}
+          onChange={(e) => handleChange(e, setRankData)}>
+          <option selected disabled>
+            select rank number
+          </option>
+          <option value={4}>4</option>
+          <option value={3}>3</option>
+          <option value={2}>2</option>
+          <option value={1}>1</option>
+        </select>
+      )}
+    </>
+  );
+
+  const regionForm = (
+    <>
+      <label htmlFor="region">Region</label>
+      <select
+        name="region"
+        value={userData.region}
+        onChange={(e) => handleChange(e, setUserData)}
+        required>
+        {['NA', 'EUW', 'EUNE', 'LAN'].map((region) => (
+          <option value={region}>{region}</option>
+        ))}
+      </select>
+    </>
+  );
+
+  let forms = [nameForm, divisionForm, regionForm];
+
+  return forms[currentFormIndex];
+}
--- client/src/screens/Intro.jsx
+++ client/src/screens/Intro.jsx
@@ -2,9 +2,9 @@ import { useEffect, useCallback } from 'react';
 import { useState, useContext } from 'react';
 import { CurrentUserContext } from '../context/currentUser';
 import { Redirect } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
 import Alert from '@material-ui/lab/Alert';
 import { Grid } from '@material-ui/core';
+import IntroForms from '../components/IntroForms';
 
 const KEYCODES = {
   ENTER: 13,
@@ -143,91 +143,6 @@ export default function Intro() {
     };
   }, [goNextStep, currentFormIndex, handleSubmit, goPreviousStep]);
 
-  const nameForm = (
-    <Grid item container justify="space-evenly">
-      <TextField
-        type="text"
-        name="name"
-        value={userData.name}
-        onChange={(e) => handleChange(e, setUserData)}
-        onKeyPress={(e) => {
-          // if user enters special characters don't do anything
-          if (!/^\w+$/.test(e.key)) e.preventDefault();
-        }}
-        placeholder="summoner name"
-        required
-      />
-      <TextField
-        type="text"
-        name="discord"
-        value={userData.discord}
-        onChange={(e) => handleChange(e, setUserData)}
-        placeholder="Discord (name and #)"
-        required
-      />
-    </Grid>
-  );
-
-  const divisionForm = (
-    <>
-      <label htmlFor="rankDivision">Rank</label>
-      <select
-        name="rankDivision"
-        required
-        value={rankData.rankDivision}
-        onChange={(e) => handleChange(e, setRankData)}>
-        {[
-          'Unranked',
-          'Iron',
-          'Bronze',
-          'Silver',
-          'Gold',
-          'Platinum',
-          'Diamond',
-          'Master',
-          'Grandmaster',
-          'Challenger',
-        ].map((value) => (
-          <option value={value}>{value}</option>
-        ))}
-      </select>
-
-      {/* exclude this number select from divisions without numbers */}
-      {divisionsWithNumbers.includes(rankData.rankDivision) && (
-        <select
-          name="rankNumber"
-          required={divisionsWithNumbers.includes(rankData.rankDivision)}
-          value={rankData.rankNumber}
-          onChange={(e) => handleChange(e, setRankData)}>
-          <option selected disabled>
-            select rank number
-          </option>
-          <option value={4}>4</option>
-          <option value={3}>3</option>
-          <option value={2}>2</option>
-          <option value={1}>1</option>
-        </select>
-      )}
-    </>
-  );
-
-  const regionForm = (
-    <>
-      <label htmlFor="region">Region</label>
-      <select
-        name="region"
-        value={userData.region}
-        onChange={(e) => handleChange(e, setUserData)}
-        required>
-        {['NA', 'EUW', 'EUNE', 'LAN'].map((region) => (
-          <option value={region}>{region}</option>
-        ))}
-      </select>
-    </>
-  );
-
-  let forms = [nameForm, divisionForm, regionForm];
-
   if (currentUser) {
     console.log('redirecting to /');
     return <Redirect to="/" />;
@@ -251,9 +166,18 @@ export default function Intro() {
 
         <form onSubmit={handleSubmit} id="form">
           <h2>Step {currentFormIndex + 1}</h2>
-          {forms[currentFormIndex]}
 
-          {currentFormIndex === forms.length - 1 ? (
+          <IntroForms
+            handleChange={handleChange}
+            currentFormIndex={currentFormIndex}
+            userData={userData}
+            setUserData={setUserData}
+            rankData={rankData}
+            setRankData={setRankData}
+            divisionsWithNumbers={divisionsWithNumbers}
+          />
+
+          {currentFormIndex === 2 ? (
             <>
               <button type="submit">Create my account</button>
               <button onClick={goPreviousStep}>Previous</button>
