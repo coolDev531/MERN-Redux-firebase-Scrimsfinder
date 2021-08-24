e760d01c546a836649217b96af1ab133467c2d05
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-24 19:57:36 -0400

    update scrim.js

--- models/scrim.js
+++ models/scrim.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const axios = require('axios');
 const toIsoString = require('../utils/toIsoString');
 
 const generatePassword = () => {
