c9c7c8cf018f4edbfc7b8806b7b1af632064b528
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-26 18:41:53 -0400

    update Tooltip.jsx

---
+++ client/src/components/shared/Tooltip.jsx
@@ -0,0 +1,21 @@
+import { makeStyles, Tooltip } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  arrow: {
+    color: theme.palette.common.white,
+  },
+  tooltip: {
+    color: '#000',
+    fontSize: ({ fontSize }) => fontSize || 'clamp(0.8rem, 4vw, 1rem)',
+    border: '1px solid #fff',
+    fontWeight: ({ fontWeight }) => fontWeight || 700,
+    backgroundColor: theme.palette.common.white,
+    boxShadow: '1px 2px 4px 1px #999',
+  },
+}));
+
+export default function CustomTooltip(props) {
+  const classes = useStyles(props);
+
+  return <Tooltip arrow classes={classes} {...props} />;
+}
