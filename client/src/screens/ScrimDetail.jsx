ee0c87fb51bd835e55b6087bf8d223805735b75a
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-29 15:10:44 -0400

    update stuff

--- client/src/screens/ScrimDetail.jsx
+++ client/src/screens/ScrimDetail.jsx
@@ -1,26 +1,50 @@
 import { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import ScrimSection from '../components/ScrimSection';
 import { getScrimById } from '../services/scrims';
 import { ScrimsContext } from '../context/scrimsContext';
 import Navbar from '../components/shared/Navbar';
 import { Helmet } from 'react-helmet';
+import LoadingGif from '../assets/images/loading.gif';
+import { Typography } from '@material-ui/core';
 
 export default function ScrimDetail() {
   const { id } = useParams();
   const [scrim, setScrim] = useState(null);
   const { scrims } = useContext(ScrimsContext);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchScrimData = async () => {
-      const scrimData = await getScrimById(id);
-      setScrim(scrimData);
+      try {
+        const scrimData = await getScrimById(id);
+        if (scrimData) {
+          setScrim(scrimData);
+        }
+        // push to / if scrim not found.
+      } catch (error) {
+        history.push('/');
+      }
     };
     fetchScrimData();
     // run this on mount and everytime scrims change.
-  }, [id, scrims]);
+  }, [id, scrims, history]);
 
-  if (!scrim) return <></>;
+  if (!scrim)
+    return (
+      <main className="page-content">
+        <div className="centered">
+          <div className="loading-wrapper">
+            <div className="content-container">
+              <Typography variant="h4" align="center" gutterBottom>
+                Loading Scrim Data...
+              </Typography>
+              <img className="loading" src={LoadingGif} alt="loading" />
+            </div>
+          </div>
+        </div>
+      </main>
+    );
 
   return (
     <div>
@@ -29,7 +53,7 @@ export default function ScrimDetail() {
         <title>LoL Scrims Finder</title>
         <meta
           name="description"
-          content={`visit ${scrim.createdBy.name}'s scrim at LoL Scrims Finder!`}
+          content="Visit this scrim at LoL Scrims Finder!"
         />
       </Helmet>
 
