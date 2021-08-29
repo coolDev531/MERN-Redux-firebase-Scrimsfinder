d80d382f8fe24ff8b49a31210376f36ad103cb75
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-29 19:07:58 -0400

    update scrim.js

--- models/scrim.js
+++ models/scrim.js
@@ -51,7 +51,7 @@ const Scrim = new Schema(
     },
     region: { type: String, default: 'NA', required: true },
     createdBy: { type: Object, required: true },
-    teamWon: { type: 'String' },
+    teamWon: { type: 'String', default: null },
   },
   { timestamps: true }
 );
