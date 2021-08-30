b725f2cafa82fe776908c9105fd7d5bde052c249
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-29 22:50:01 -0400

    update Navbar

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -21,6 +21,7 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import moment from 'moment';
 import 'moment-timezone';
 import AdminArea from './AdminArea';
+import HideOnScroll from './HideOnScroll';
 
 const useStyles = makeStyles((theme) => ({
   offset: theme.mixins.offset,
@@ -70,164 +71,167 @@ export default function Navbar({
 
   return (
     <>
-      <AppBar className="page-section site-header" position="sticky">
-        <Toolbar>
-          <div className="inner-column">
-            <div className="d-flex align-center justify-between">
-              <div className="logo d-flex align-center">
-                {/* need license to use img */}
-                {/* <img
+      <HideOnScroll>
+        <AppBar className="page-section site-header" position="sticky">
+          <Toolbar>
+            <div className="inner-column">
+              <div className="d-flex align-center justify-between">
+                <div className="logo d-flex align-center">
+                  {/* need license to use img */}
+                  {/* <img
                   src={BOOTCAMP_LOL_SRC}
                   alt="logo"
                   style={{ marginRight: '10px' }}
                 /> */}
-                &nbsp;
-                <Link to="/" className="link">
-                  <h1>LoL Scrims finder</h1>
-                </Link>
-              </div>
+                  &nbsp;
+                  <Link to="/" className="link">
+                    <h1>LoL Scrims finder</h1>
+                  </Link>
+                </div>
 
-              <div className="d-flex mr-3">
-                {pathname !== '/scrims/new' ? (
-                  <AdminArea>
+                <div className="d-flex mr-3">
+                  {pathname !== '/scrims/new' ? (
+                    <AdminArea>
+                      <Button
+                        className="mr-3"
+                        variant="contained"
+                        color="primary"
+                        onClick={() => history.push('/scrims/new')}>
+                        Create Scrim
+                      </Button>
+                    </AdminArea>
+                  ) : (
                     <Button
                       className="mr-3"
                       variant="contained"
                       color="primary"
-                      onClick={() => history.push('/scrims/new')}>
-                      Create Scrim
+                      onClick={() => history.goBack()}>
+                      Go Back
                     </Button>
-                  </AdminArea>
-                ) : (
+                  )}
+                  <Box marginRight={2} />
+                  &nbsp;
                   <Button
-                    className="mr-3"
+                    onClick={handleLogOut}
                     variant="contained"
-                    color="primary"
-                    onClick={() => history.goBack()}>
-                    Go Back
+                    color="secondary">
+                    Log Out
                   </Button>
-                )}
-                <Box marginRight={2} />
-                &nbsp;
-                <Button
-                  onClick={handleLogOut}
-                  variant="contained"
-                  color="secondary">
-                  Log Out
-                </Button>
-              </div>
-            </div>
-            <br />
-            {!showLess && (
-              <Grid container direction="row" justify="space-between">
-                <div>
-                  <h2>Welcome: {currentUser?.name}</h2>
                 </div>
-                {showCheckboxes && (
-                  <div className="d-flex align-center">
-                    <FormGroup row className="text-white">
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={hideCurrentScrims}
-                            color="primary"
-                            onChange={() =>
-                              setHideCurrentScrims((prevState) => !prevState)
-                            }
-                            name="hideCurrentScrims"
-                          />
-                        }
-                        label="Hide current scrims"
-                        labelPlacement="bottom"
-                      />
-
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={hideUpcomingScrims}
-                            color="primary"
-                            onChange={() =>
-                              setHideUpcomingScrims((prevState) => !prevState)
-                            }
-                            name="hideUpcomingScrims"
-                          />
-                        }
-                        label="Hide upcoming scrims"
-                        labelPlacement="bottom"
-                      />
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            color="primary"
-                            checked={hidePreviousScrims}
-                            onChange={() =>
-                              setHidePreviousScrims((prevState) => !prevState)
-                            }
-                            name="hidePreviousScrims"
-                          />
-                        }
-                        label="Hide Previous Scrims"
-                        labelPlacement="bottom"
-                      />
-                    </FormGroup>
+              </div>
+              <br />
+              {!showLess && (
+                <Grid container direction="row" justify="space-between">
+                  <div>
+                    <h2>Welcome: {currentUser?.name}</h2>
                   </div>
-                )}
-                {showDropdowns && (
-                  <div
-                    id="nav__selects--container"
-                    className="d-flex align-center">
-                    <div id="nav__date-filter--container">
-                      <TextField
-                        id="date"
-                        required
-                        label="Scrims Date"
-                        type="date"
-                        name="scrimsDate"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        value={moment(scrimsDate).format('yyyy-MM-DD')}
-                        onChange={(e) => {
-                          setScrimsDate(
-                            new Date(e.target.value.replace('-', '/'))
-                          );
-                        }}
-                      />
+                  {showCheckboxes && (
+                    <div className="d-flex align-center">
+                      <FormGroup row className="text-white">
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={hideCurrentScrims}
+                              color="primary"
+                              onChange={() =>
+                                setHideCurrentScrims((prevState) => !prevState)
+                              }
+                              name="hideCurrentScrims"
+                            />
+                          }
+                          label="Hide current scrims"
+                          labelPlacement="bottom"
+                        />
 
-                      <FormHelperText className="text-white">
-                        Filter scrims by date
-                      </FormHelperText>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={hideUpcomingScrims}
+                              color="primary"
+                              onChange={() =>
+                                setHideUpcomingScrims((prevState) => !prevState)
+                              }
+                              name="hideUpcomingScrims"
+                            />
+                          }
+                          label="Hide upcoming scrims"
+                          labelPlacement="bottom"
+                        />
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              color="primary"
+                              checked={hidePreviousScrims}
+                              onChange={() =>
+                                setHidePreviousScrims((prevState) => !prevState)
+                              }
+                              name="hidePreviousScrims"
+                            />
+                          }
+                          label="Hide Previous Scrims"
+                          labelPlacement="bottom"
+                        />
+                      </FormGroup>
                     </div>
-                    <Box marginRight={4} />
+                  )}
+                  {showDropdowns && (
+                    <div
+                      id="nav__selects--container"
+                      className="d-flex align-center">
+                      <div id="nav__date-filter--container">
+                        <TextField
+                          id="date"
+                          required
+                          label="Scrims Date"
+                          type="date"
+                          name="scrimsDate"
+                          InputLabelProps={{
+                            shrink: true,
+                          }}
+                          value={moment(scrimsDate).format('yyyy-MM-DD')}
+                          onChange={(e) => {
+                            setScrimsDate(
+                              new Date(e.target.value.replace('-', '/'))
+                            );
+                          }}
+                        />
 
-                    <div id="nav__region-filter--container">
-                      <InputLabel className="text-white">Region</InputLabel>
+                        <FormHelperText className="text-white">
+                          Filter scrims by date
+                        </FormHelperText>
+                      </div>
+                      <Box marginRight={4} />
 
-                      <Select
-                        value={scrimsRegion}
-                        className="text-white"
-                        onChange={(e) => {
-                          const region = e.target.value;
-                          toggleFetch((prev) => !prev);
-                          setScrimsRegion(region); // set the navbar select value to selected region
-                        }}>
-                        {selectRegions.map((region, key) => (
-                          <MenuItem value={region} key={key}>
-                            {region}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                      <FormHelperText className="text-white">
-                        Filter scrims by region
-                      </FormHelperText>
+                      <div id="nav__region-filter--container">
+                        <InputLabel className="text-white">Region</InputLabel>
+
+                        <Select
+                          value={scrimsRegion}
+                          className="text-white"
+                          onChange={(e) => {
+                            const region = e.target.value;
+                            toggleFetch((prev) => !prev);
+                            setScrimsRegion(region); // set the navbar select value to selected region
+                          }}>
+                          {selectRegions.map((region, key) => (
+                            <MenuItem value={region} key={key}>
+                              {region}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                        <FormHelperText className="text-white">
+                          Filter scrims by region
+                        </FormHelperText>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </Grid>
-            )}
-          </div>
-        </Toolbar>
-      </AppBar>
+                  )}
+                </Grid>
+              )}
+            </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+
       <div className={classes.offset} />
       <div className={classes.toolbarDistance} />
     </>
