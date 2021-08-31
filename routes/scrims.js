bcc03465125d83890de68ec64ad3f30eeb5a9512
HaradaYasuo <academic.dev0531@gmail.com>
2021-08-31 16:29:20 -0400

    update server.js update way apikey middleware is used

--- routes/scrims.js
+++ routes/scrims.js
@@ -1,33 +1,16 @@
 const { Router } = require('express');
 const controllers = require('../controllers/scrims');
-const apiKey = require('../utils/apiKey');
 
 const router = Router();
 
-router.get('/scrims', apiKey, controllers.getAllScrims); // GET
-router.post('/scrims', apiKey, controllers.createScrim); // POST
-router.get('/scrims/:id', apiKey, controllers.getScrimById); // GET
-router.put('/scrims/:id', apiKey, controllers.updateScrim); // PUT
-router.put(
-  '/scrims/:id/insert-player',
-  apiKey,
-  controllers.insertPlayerInScrim
-); // PUT
-router.put(
-  '/scrims/:id/remove-player',
-  apiKey,
-  controllers.removePlayerFromScrim
-); // PUT
-router.put(
-  '/scrims/:id/insert-caster',
-  apiKey,
-  controllers.insertCasterInScrim
-); // PUT
-router.put(
-  '/scrims/:id/remove-caster',
-  apiKey,
-  controllers.removeCasterFromScrim
-); // PUT
-router.delete('/scrims/:id', apiKey, controllers.deleteScrim); // DELETE
+router.get('/scrims', controllers.getAllScrims); // GET
+router.post('/scrims', controllers.createScrim); // POST
+router.get('/scrims/:id', controllers.getScrimById); // GET
+router.put('/scrims/:id', controllers.updateScrim); // PUT
+router.put('/scrims/:id/insert-player', controllers.insertPlayerInScrim); // PUT
+router.put('/scrims/:id/remove-player', controllers.removePlayerFromScrim); // PUT
+router.put('/scrims/:id/insert-caster', controllers.insertCasterInScrim); // PUT
+router.put('/scrims/:id/remove-caster', controllers.removeCasterFromScrim); // PUT
+router.delete('/scrims/:id', controllers.deleteScrim); // DELETE
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 const db = require('./db/connection');
 const scrimRoutes = require('./routes/scrims');
 const bodyParser = require('body-parser');
+const apiKey = require('./utils/apiKey');
 
 const app = express();
 
@@ -12,13 +13,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+// this route doesn't need an api key because app.use(apikey) is called later
 app.get('/', (_req, res) => {
   res.send(
     '<h1>LOL BOOTCAMP SCRIMS FINDER</h1> <h2>How to use: go to /api/scrims to find all scrims.</h2>'
   );
 });
 
+// require an api key for these routes
+app.use(apiKey);
 app.use('/api', scrimRoutes);
+
+// another way to require api key for a specific route only.
+// router.get('/scrims', apiKey, controllers.getAllScrims);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
