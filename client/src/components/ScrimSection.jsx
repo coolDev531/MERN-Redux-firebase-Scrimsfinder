b2e95c8c766300712045f944562bbc2f1b15b0ca
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-03 19:22:11 -0400

    some grammar fixes

--- client/src/components/ScrimSection.jsx
+++ client/src/components/ScrimSection.jsx
@@ -482,8 +482,26 @@ export default function ScrimSection({ scrim, isInDetail }) {
                       {`${teamOne.length + teamTwo.length}/10`}
                     </h2>
                     <h5 className="text-black">
-                      Please get {teamOneDifference} players in team one <br />
-                      and {teamTwoDifference} players in team two <br />
+                      {teamOneDifference > 0 ? (
+                        <>
+                          {' '}
+                          Please get {teamOneDifference}{' '}
+                          {`player${teamOneDifference > 1 ? 's' : ''}`} in Team
+                          1
+                          <br />
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                      {teamTwoDifference > 0 ? (
+                        <>
+                          {teamOneDifference < 0 ? 'and' : ''}{' '}
+                          {teamTwoDifference}{' '}
+                          {`player${teamTwoDifference > 1 ? 's' : ''}`} in Team
+                          2
+                          <br />
+                        </>
+                      ) : null}
                       to unlock lobby name and password
                     </h5>
                     {scrim.createdBy?.email === currentUser?.email ? (
