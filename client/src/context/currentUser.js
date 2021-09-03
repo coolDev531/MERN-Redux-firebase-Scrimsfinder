ddbe1f7201de1383589607ccd09a24861bacd08c
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-02 23:32:32 -0400

    update currentUser add google verify

--- client/src/context/currentUser.js
+++ client/src/context/currentUser.js
@@ -1,6 +1,7 @@
 import { useState, createContext, useEffect } from 'react';
 import { verifyUser } from '../services/auth';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import { auth } from '../firebase';
 
 const CurrentUserContext = createContext();
 
@@ -11,13 +12,15 @@ function CurrentUserProvider({ children }) {
     return user !== null ? user : null;
   });
 
+  const { pathname } = useLocation();
+
   const history = useHistory();
 
   useEffect(() => {
     localStorage.setItem('currentUser', JSON.stringify(currentUser));
   }, [currentUser]);
 
-  // verify user details
+  // verify user details in mongodb database.
   useEffect(() => {
     const handleVerifyUser = async () => {
       let googleParams = {
@@ -25,6 +28,7 @@ function CurrentUserProvider({ children }) {
         email: currentUser?.email,
       };
 
+      // back-end is taking google data and checking if it exists.
       const verifiedUser = await verifyUser(googleParams);
 
       if (verifiedUser) {
@@ -36,6 +40,31 @@ function CurrentUserProvider({ children }) {
     };
     handleVerifyUser();
   }, [history, currentUser?.uid, currentUser?.email]);
+
+  //  check if user is signed in with google (verification)
+  useEffect(() => {
+    // don't do this at login because it will push to home after signing in with google.
+    if (pathname.includes('/user-setup')) return;
+
+    if (!currentUser?._id ?? false) return; // don't continue if user didn't sign up or in yet.
+
+    auth.onAuthStateChanged(async (gmailData) => {
+      if (gmailData) {
+        // User is signed in with google.
+        setCurrentUser((prevState) => ({
+          ...prevState,
+          email: gmailData.email,
+          uid: gmailData.uid,
+        }));
+      } else {
+        setCurrentUser(null);
+        history.push('/user-setup');
+        // No user is signed in with google.
+      }
+    });
+
+    //eslint-disable-next-line
+  }, [pathname]);
 
   return (
     <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
