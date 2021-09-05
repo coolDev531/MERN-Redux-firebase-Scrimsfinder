37d487dec62e705d9fa61e484436e62afd0731f1
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-05 16:21:01 -0400

    update navbar.jsx

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -3,50 +3,30 @@ import { useState, useEffect } from 'react';
 import { useAuth } from '../../context/currentUser';
 
 // components
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
 import {
-  Box,
   Button,
-  FormHelperText,
-  TextField,
   Grid,
   makeStyles,
   AppBar,
   Toolbar,
-  FormGroup,
-  FormControlLabel,
-  Checkbox,
-  Drawer,
-  List,
-  ListItem,
-  Divider,
-  ListItemText,
-  ListItemIcon,
   Typography,
   IconButton,
 } from '@material-ui/core';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import { BOOTCAMP_LOL_SRC } from '../../utils/bootcampImg'; // need license
 import 'moment-timezone';
-import AdminArea from './AdminArea';
 import HideOnScroll from './HideOnScroll';
 import { InnerColumn } from './PageComponents';
 import Tooltip from './Tooltip';
 
 // utils
 import { KEYCODES } from './../../utils/keycodes';
-import moment from 'moment';
-import clsx from 'clsx';
 
 // icons
-import SettingsIcon from '@material-ui/icons/Settings';
 import KeyIcon from '@material-ui/icons/VpnKey';
-import ExitIcon from '@material-ui/icons/ExitToApp';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import CreateIcon from '@material-ui/icons/BorderColor';
 import MenuIcon from '@material-ui/icons/Menu'; // burger icon
+import NavbarDrawer from './NavbarDrawer';
+
 const useStyles = makeStyles((theme) => ({
   offset: theme.mixins.offset,
   toolbarDistance: theme.mixins.toolbar,
@@ -60,15 +40,7 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: '30px',
     paddingBottom: '20px',
   },
-  drawerList: {
-    width: 250,
-  },
-  drawerFullList: {
-    width: 'auto',
-  },
 }));
-
-const DRAWER_ANCHOR = 'top';
 
 export default function Navbar({
   toggleFetch,
@@ -82,50 +54,8 @@ export default function Navbar({
   hideProps,
 }) {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const history = useHistory();
-  const { pathname } = useLocation();
-  const { currentUser, logOutUser, logInUser } = useAuth();
+  const { currentUser, logInUser } = useAuth();
   const classes = useStyles();
-
-  let allRegions = ['NA', 'EUW', 'EUNE', 'LAN'];
-
-  let selectRegions = [
-    currentUser?.region,
-    ...allRegions.filter((r) => r !== currentUser?.region),
-  ];
-
-  // this is terrible but I'm doing it this way because it will cause an error that it can't find props of uudefined
-  let hidePreviousScrims = hideProps?.hidePreviousScrims,
-    hideCurrentScrims = hideProps?.hideCurrentScrims,
-    hideUpcomingScrims = hideProps?.hideUpcomingScrims,
-    setHidePreviousScrims = hideProps?.setHidePreviousScrims,
-    setHideCurrentScrims = hideProps?.setHideCurrentScrims,
-    setHideUpcomingScrims = hideProps?.setHideUpcomingScrims;
-
-  const drawerNavPush = (path) => {
-    setIsDrawerOpen(false);
-
-    // using settimeout so the user sees the drawer close before the path gets redirected.
-    setTimeout(() => {
-      history.push(path);
-    }, 100);
-  };
-
-  useEffect(() => {
-    const handleKeyUp = ({ keycode }) => {
-      // close drawer if user is pressing escape
-      if (keycode === KEYCODES.ESCAPE) {
-        if (isDrawerOpen) {
-          setIsDrawerOpen(false);
-          return;
-        }
-      }
-    };
-    document.addEventListener('keyup', handleKeyUp);
-    return () => {
-      document.removeEventListener('keyup', handleKeyUp);
-    };
-  }, [isDrawerOpen]);
 
   return (
     <>
@@ -200,181 +130,19 @@ export default function Navbar({
         </AppBar>
       </HideOnScroll>
 
-      {/* Additional navbar (drawer). */}
-      <Drawer
-        anchor={DRAWER_ANCHOR}
-        open={isDrawerOpen}
-        onClose={() => setIsDrawerOpen(false)}>
-        <InnerColumn>
-          <div
-            className={clsx(classes.drawerList, {
-              [classes.drawerFullList]:
-                DRAWER_ANCHOR === 'top' || DRAWER_ANCHOR === 'bottom',
-            })}>
-            <List>
-              {/* Settings button */}
-              <ListItem button onClick={() => drawerNavPush('/settings')}>
-                <ListItemIcon>
-                  <SettingsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Settings" />
-              </ListItem>
-
-              <Divider />
-
-              {/* Create scrim button (admins only) */}
-              <AdminArea>
-                <ListItem button onClick={() => drawerNavPush('/scrims/new')}>
-                  <ListItemIcon>
-                    <CreateIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Create Scrim" />
-                </ListItem>
-                <Divider />
-              </AdminArea>
-
-              {/* Log out button */}
-              {currentUser?.uid && (
-                <>
-                  <ListItem button onClick={logOutUser}>
-                    <ListItemIcon>
-                      <ExitIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Log Out" />
-                  </ListItem>
-                </>
-              )}
-              {/* don't show divider if there isn't anything else to show below... */}
-              {showCheckboxes && <Divider />}
-            </List>
-          </div>
-
-          {!showLess && (
-            <>
-              <Grid
-                container
-                alignItems="center"
-                direction="row"
-                justifyContent="space-between"
-                style={{ padding: '15px 10px 10px 10px' }}>
-                {showDropdowns && (
-                  <Grid
-                    item
-                    container
-                    xs={6}
-                    alignItems="center"
-                    id="nav__selects--container">
-                    {/* date regions and filters */}
-                    <Grid item>
-                      <TextField
-                        id="date"
-                        required
-                        label="Scrims Date"
-                        type="date"
-                        name="scrimsDate"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        value={
-                          moment(new Date(scrimsDate)).format('yyyy-MM-DD') ||
-                          moment().format('yyyy-MM-DD')
-                        }
-                        onChange={(e) => {
-                          setScrimsDate(moment(e.target.value));
-                        }}
-                      />
-
-                      <FormHelperText className="text-white">
-                        Filter scrims by date
-                      </FormHelperText>
-                    </Grid>
-
-                    <Box marginRight={4} />
-
-                    <Grid item id="nav__region-filter--container">
-                      <InputLabel className="text-white">Region</InputLabel>
-
-                      <Select
-                        value={scrimsRegion}
-                        className="text-white"
-                        onChange={(e) => {
-                          const region = e.target.value;
-                          toggleFetch((prev) => !prev);
-                          setScrimsRegion(region); // set the navbar select value to selected region
-                        }}>
-                        {selectRegions.map((region, key) => (
-                          <MenuItem value={region} key={key}>
-                            {region}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                      <FormHelperText className="text-white">
-                        Filter scrims by region
-                      </FormHelperText>
-                    </Grid>
-                  </Grid>
-                )}
-
-                {/* Show scrims (current, previous, upcoming) buttons */}
-                {showCheckboxes && (
-                  <Grid item alignItems="center" container xs={6}>
-                    <FormGroup
-                      row
-                      className="text-white"
-                      style={{ justifyContent: 'center' }}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            // the UI says "show X scrims", so in this case we are reversing the boolean for checked, lol.
-                            // doesn't matter functionally.
-                            checked={!hideCurrentScrims}
-                            color="primary"
-                            onChange={() =>
-                              setHideCurrentScrims((prevState) => !prevState)
-                            }
-                            name="hideCurrentScrims"
-                          />
-                        }
-                        label="Show current scrims"
-                        labelPlacement="bottom"
-                      />
-
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={!hideUpcomingScrims}
-                            color="primary"
-                            onChange={() =>
-                              setHideUpcomingScrims((prevState) => !prevState)
-                            }
-                            name="hideUpcomingScrims"
-                          />
-                        }
-                        label="Show upcoming scrims"
-                        labelPlacement="bottom"
-                      />
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            color="primary"
-                            checked={!hidePreviousScrims}
-                            onChange={() =>
-                              setHidePreviousScrims((prevState) => !prevState)
-                            }
-                            name="hidePreviousScrims"
-                          />
-                        }
-                        label="Show Previous Scrims"
-                        labelPlacement="bottom"
-                      />
-                    </FormGroup>
-                  </Grid>
-                )}
-              </Grid>
-            </>
-          )}
-        </InnerColumn>
-      </Drawer>
+      <NavbarDrawer
+        isDrawerOpen={isDrawerOpen}
+        setIsDrawerOpen={setIsDrawerOpen}
+        showCheckboxes={showCheckboxes}
+        showDropdowns={showDropdowns}
+        showLess={showLess}
+        hideProps={hideProps}
+        scrimsRegion={scrimsRegion}
+        setScrimsRegion={setScrimsRegion}
+        scrimsDate={scrimsDate}
+        setScrimsDate={setScrimsDate}
+        fetchScrims={() => toggleFetch((prev) => !prev)}
+      />
 
       <div className={classes.offset} />
       <div className={classes.toolbarDistance} />
---
+++ client/src/components/shared/NavbarDrawer.jsx
@@ -0,0 +1,288 @@
+// hooks
+import { useEffect } from 'react';
+import { useAuth } from './../../context/currentUser';
+import { useHistory } from 'react-router-dom';
+
+// components
+import { InnerColumn } from './PageComponents';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import {
+  Box,
+  FormHelperText,
+  TextField,
+  Grid,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Drawer,
+  List,
+  ListItem,
+  Divider,
+  ListItemText,
+  ListItemIcon,
+  makeStyles,
+} from '@material-ui/core';
+import AdminArea from './AdminArea';
+
+// icons
+import SettingsIcon from '@material-ui/icons/Settings';
+import ExitIcon from '@material-ui/icons/ExitToApp';
+import CreateIcon from '@material-ui/icons/BorderColor';
+// utils
+import moment from 'moment';
+import clsx from 'clsx';
+import { KEYCODES } from '../../utils/keycodes';
+
+const DRAWER_ANCHOR = 'top';
+
+const useStyles = makeStyles({
+  drawerList: {
+    width: 250,
+  },
+  drawerFullList: {
+    width: 'auto',
+  },
+});
+
+export default function NavbarDrawer({
+  isDrawerOpen,
+  setIsDrawerOpen,
+  showCheckboxes,
+  showDropdowns,
+  showLess,
+  hideProps,
+  scrimsRegion, // the region the scrims are going to be filtered by
+  setScrimsRegion,
+  scrimsDate, // the date the scrims are going to be filtered by
+  setScrimsDate,
+  fetchScrims,
+}) {
+  const classes = useStyles();
+  const { currentUser, logOutUser } = useAuth();
+  const history = useHistory();
+  let allRegions = ['NA', 'EUW', 'EUNE', 'LAN'];
+
+  let selectRegions = [
+    currentUser?.region,
+    ...allRegions.filter((r) => r !== currentUser?.region),
+  ];
+
+  // this is terrible but I'm doing it this way because it will cause an error that it can't find props of undefined
+  let hidePreviousScrims = hideProps?.hidePreviousScrims,
+    hideCurrentScrims = hideProps?.hideCurrentScrims,
+    hideUpcomingScrims = hideProps?.hideUpcomingScrims,
+    setHidePreviousScrims = hideProps?.setHidePreviousScrims,
+    setHideCurrentScrims = hideProps?.setHideCurrentScrims,
+    setHideUpcomingScrims = hideProps?.setHideUpcomingScrims;
+
+  const drawerNavPush = (path) => {
+    setIsDrawerOpen(false);
+
+    // using settimeout so the user sees the drawer close before the path gets redirected.
+    setTimeout(() => {
+      history.push(path);
+    }, 100);
+  };
+
+  const onSelectRegion = (e) => {
+    const region = e.target.value;
+    fetchScrims(); // not necessary, trying to ping the server.
+    setScrimsRegion(region); // set the navbar select value to selected region
+  };
+
+  const onSelectDate = (e) => {
+    setScrimsDate(moment(e.target.value));
+  };
+
+  useEffect(() => {
+    const handleKeyUp = ({ keycode }) => {
+      // close drawer if user is pressing escape
+      if (keycode === KEYCODES.ESCAPE) {
+        if (isDrawerOpen) {
+          setIsDrawerOpen(false);
+          return;
+        }
+      }
+    };
+    document.addEventListener('keyup', handleKeyUp);
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+
+    // it's asking to add a dep for the setter, disabling it with comment.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isDrawerOpen]);
+
+  return (
+    <Drawer
+      anchor={DRAWER_ANCHOR}
+      open={isDrawerOpen}
+      onClose={() => setIsDrawerOpen(false)}>
+      <InnerColumn>
+        <div
+          className={clsx(classes.drawerList, {
+            [classes.drawerFullList]:
+              DRAWER_ANCHOR === 'top' || DRAWER_ANCHOR === 'bottom',
+          })}>
+          <List>
+            {/* Settings button */}
+            <ListItem button onClick={() => drawerNavPush('/settings')}>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItem>
+
+            <Divider />
+
+            {/* Create scrim button (admins only) */}
+            <AdminArea>
+              <ListItem button onClick={() => drawerNavPush('/scrims/new')}>
+                <ListItemIcon>
+                  <CreateIcon />
+                </ListItemIcon>
+                <ListItemText primary="Create Scrim" />
+              </ListItem>
+              <Divider />
+            </AdminArea>
+
+            {/* Log out button */}
+            {currentUser?.uid && (
+              <>
+                <ListItem button onClick={logOutUser}>
+                  <ListItemIcon>
+                    <ExitIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Log Out" />
+                </ListItem>
+              </>
+            )}
+            {/* don't show divider if there isn't anything else to show below... */}
+            {showCheckboxes && <Divider />}
+          </List>
+        </div>
+
+        {!showLess && (
+          <>
+            <Grid
+              container
+              alignItems="center"
+              direction="row"
+              justifyContent="space-between"
+              style={{ padding: '15px 10px 10px 10px' }}>
+              {showDropdowns && (
+                <Grid
+                  item
+                  container
+                  xs={6}
+                  alignItems="center"
+                  id="nav__selects--container">
+                  {/* date regions and filters */}
+                  <Grid item>
+                    <TextField
+                      id="date"
+                      required
+                      label="Scrims Date"
+                      type="date"
+                      name="scrimsDate"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      value={
+                        moment(new Date(scrimsDate)).format('yyyy-MM-DD') ||
+                        moment().format('yyyy-MM-DD')
+                      }
+                      onChange={onSelectDate}
+                    />
+
+                    <FormHelperText className="text-white">
+                      Filter scrims by date
+                    </FormHelperText>
+                  </Grid>
+
+                  <Box marginRight={4} />
+
+                  <Grid item id="nav__region-filter--container">
+                    <InputLabel className="text-white">Region</InputLabel>
+
+                    <Select
+                      value={scrimsRegion}
+                      className="text-white"
+                      onChange={onSelectRegion}>
+                      {selectRegions.map((region, key) => (
+                        <MenuItem value={region} key={key}>
+                          {region}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    <FormHelperText className="text-white">
+                      Filter scrims by region
+                    </FormHelperText>
+                  </Grid>
+                </Grid>
+              )}
+
+              {/* Show scrims (current, previous, upcoming) buttons */}
+              {showCheckboxes && (
+                <Grid item alignItems="center" container xs={6}>
+                  <FormGroup
+                    row
+                    className="text-white"
+                    style={{ justifyContent: 'center' }}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          // the UI says "show X scrims", so in this case we are reversing the boolean for checked, lol.
+                          // doesn't matter functionally.
+                          checked={!hideCurrentScrims}
+                          color="primary"
+                          onChange={() =>
+                            setHideCurrentScrims((prevState) => !prevState)
+                          }
+                          name="hideCurrentScrims"
+                        />
+                      }
+                      label="Show current scrims"
+                      labelPlacement="bottom"
+                    />
+
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={!hideUpcomingScrims}
+                          color="primary"
+                          onChange={() =>
+                            setHideUpcomingScrims((prevState) => !prevState)
+                          }
+                          name="hideUpcomingScrims"
+                        />
+                      }
+                      label="Show upcoming scrims"
+                      labelPlacement="bottom"
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          color="primary"
+                          checked={!hidePreviousScrims}
+                          onChange={() =>
+                            setHidePreviousScrims((prevState) => !prevState)
+                          }
+                          name="hidePreviousScrims"
+                        />
+                      }
+                      label="Show Previous Scrims"
+                      labelPlacement="bottom"
+                    />
+                  </FormGroup>
+                </Grid>
+              )}
+            </Grid>
+          </>
+        )}
+      </InnerColumn>
+    </Drawer>
+  );
+}
