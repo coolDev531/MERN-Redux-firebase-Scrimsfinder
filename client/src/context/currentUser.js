85aceeb57c1201875df0617c4f4e38d60a958a9c
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-04 21:09:33 -0400

    update currentUser

--- client/src/context/currentUser.js
+++ client/src/context/currentUser.js
@@ -1,6 +1,5 @@
 import { useState, createContext, useEffect } from 'react';
 import { verifyUser } from '../services/auth';
-import { useHistory } from 'react-router-dom';
 import { auth } from '../firebase';
 
 const CurrentUserContext = createContext();
@@ -8,33 +7,6 @@ const CurrentUserContext = createContext();
 function CurrentUserProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  const history = useHistory();
-
-  useEffect(() => {
-    localStorage.setItem('currentUser', JSON.stringify(currentUser));
-  }, [currentUser]);
-
-  // verify user details in mongodb database.
-  // useEffect(() => {
-  //   const handleVerifyUser = async () => {
-  //     let googleParams = {
-  //       uid: currentUser?.uid, // google id
-  //       email: currentUser?.email,
-  //     };
-
-  //     // back-end is taking google data and checking if it exists.
-  //     const verifiedUser = await verifyUser(googleParams);
-
-  //     if (verifiedUser) {
-  //       setCurrentUser(verifiedUser);
-  //     } else {
-  //       setCurrentUser(null);
-  //       history.push('/user-setup');
-  //     }
-  //   };
-  //   handleVerifyUser();
-  // }, [history, currentUser?.uid, currentUser?.email]);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (googleUser) => {
