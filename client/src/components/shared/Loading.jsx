5074f344c907c9c630438237930caf71dfbbcfbf
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-06 11:55:56 -0400

    update Loading to show text when heroku is hibernating

--- client/src/components/shared/Loading.jsx
+++ client/src/components/shared/Loading.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState, useRef, useMemo } from 'react';
 import { Typography } from '@material-ui/core';
 import LoadingGif from '../../assets/images/loading.gif';
 import { PageContent } from './PageComponents';
 
 export default function Loading({ text }) {
+  const [isHerokuHibernating, setIsHerokuHibernating] = useState(false);
+  let isMounted = useRef(true);
+
+  const IS_SLEEPING_INTERVAL = 6000; // 6 seconds means heroku is hibernating
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!isMounted.current) return;
+      setIsHerokuHibernating(true);
+    }, IS_SLEEPING_INTERVAL);
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const displayText = useMemo(
+    () => (isHerokuHibernating ? 'Waking up the server...' : text),
+    [isHerokuHibernating, text]
+  );
+
   return (
     <PageContent>
       <div className="centered">
         <div className="loading-wrapper">
           <div className="content-container">
             <Typography variant="h4" align="center" gutterBottom>
-              {text}
+              {displayText}
             </Typography>
             <img className="loading" src={LoadingGif} alt="loading" />
           </div>
