d9824b10a3a1a4474a1cd0249f7cc61df5c37fc6
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-06 18:21:06 -0400

    update truncate.js

---
+++ client/src/utils/truncate.js
@@ -0,0 +1,13 @@
+/**
+ * @method truncate
+ *  truncate string when it's length is greater than the n arguement
+ * @param {String} str
+ * @param {Number} n
+ * @return {string}
+ */
+// this is made specifically for MenuItem text overflow in ScrimTeamList.jsx.
+export const truncate = (str, n) => {
+  if (typeof str === 'string') {
+    return str?.length > n ? `${str?.substr(0, n - 1)} ...` : str ?? '';
+  }
+};
