f8afa5fda334bd46aa4809ccb18944ac60b0dbb3
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-08 08:07:50 -0400

    update generatePassword

--- utils/generatePassword.js
+++ utils/generatePassword.js
@@ -1,12 +1,21 @@
+/**
+ * @method generatePassword
+  generates a random password 
+  is asyncrhonus because of being used in the creation of a scrim, wait for pass to be generated before continuing.
+ * @return {String} returns the result password.
+ */
 const generatePassword = async () => {
-  var pass = '';
-  var str =
+  const CHARACTERS_LENGTH = 8;
+
+  let pass = '';
+
+  let str =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
 
-  for (i = 1; i <= 8; i++) {
-    var char = Math.floor(Math.random() * str.length + 1);
+  for (i = 1; i <= CHARACTERS_LENGTH; i++) {
+    let randomIndex = Math.floor(Math.random() * str.length + 1);
 
-    pass += str.charAt(char);
+    pass += str.charAt(randomIndex);
   }
 
   return pass;
