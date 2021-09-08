959b25e7d34cfd753411e8b4bc6cc0430d21e17c
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-08 12:23:50 -0400

    update firebase.js fix SDK console warning

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,10 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth'; // for authentication
+import 'firebase/storage'; // for storage
+import 'firebase/database'; // for realtime database
+import 'firebase/firestore'; // for cloud firestore
+import 'firebase/messaging'; // for cloud messaging
+import 'firebase/functions'; // for cloud functions
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
