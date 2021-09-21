51e7b7e17e6dea6aa71168baf951c6d52dc7456b
HaradaYasuo <academic.dev0531@gmail.com>
2021-09-21 17:06:37 -0400

    update useToggle

--- client/src/hooks/useToggle.js
+++ client/src/hooks/useToggle.js
@@ -1,4 +1,5 @@
 import { useState, useCallback } from 'react';
+
 export default function useToggle(initialValue = false) {
   const [value, setValue] = useState(initialValue);
   const toggle = useCallback(() => {
