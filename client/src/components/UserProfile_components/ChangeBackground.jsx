d88820b94fa635f18db482a5ebbf5556b8159da5
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-10 22:26:49 -0400

    update ChangeBackground add save btn/

--- client/src/components/UserProfile_components/ChangeBackground.jsx
+++ client/src/components/UserProfile_components/ChangeBackground.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+
 // components
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/FormControl';
 import FormGroup from '@mui/material/FormGroup';
+import Button from '@mui/material/Button';
 
 // services
 import { updateUser } from './../../services/auth';
@@ -13,58 +15,48 @@ import { updateUser } from './../../services/auth';
 // utils
 import devLog from './../../utils/devLog';
 
+// icons
+import SaveIcon from '@mui/icons-material/Create';
+
 export default function ChangeBackground({ userBg, setUserBg, userId }) {
-  const [disabled, setDisabled] = useState(false);
+  const [buttonsDisabled, setButtonsDisabled] = useState(false);
   const { currentUser } = useSelector(({ auth }) => auth);
+
   const handleChangeImage = async (e) => {
     if (currentUser._id !== userId) return;
-    const bgImg = e.target.value;
-    setDisabled(true);
+    const bgImgValue = e.target.value;
 
     setUserBg((prevState) => ({
       ...prevState,
-      img: bgImg,
+      img: bgImgValue,
     }));
-
-    setTimeout(async () => {
-      const updatedUser = await updateUser(userId, {
-        ...currentUser,
-        profileBackgroundImg: bgImg,
-      });
-
-      devLog('updated user background', updatedUser.user.profileBackgroundImg);
-
-      setTimeout(() => {
-        setDisabled(false);
-      }, 1000);
-    }, 200);
   };
 
   const handleChangeBlur = async (e) => {
     if (currentUser._id !== userId) return;
-    setDisabled(true);
     let blurValue = e.target.value;
 
     setUserBg((prevState) => ({
       ...prevState,
       blur: blurValue,
     }));
+  };
 
-    setTimeout(async () => {
-      let updatedUser = await updateUser(userId, {
-        ...currentUser,
-        profileBackgroundBlur: blurValue,
-      });
+  const onSaveBackground = async () => {
+    const blurValue = userBg.blur ?? '20';
+    const bgImgValue = userBg.img ?? 'Summoners Rift';
 
-      devLog(
-        'updated user background blur',
-        updatedUser.user.profileBackgroundBlur
-      );
+    setButtonsDisabled(true);
 
-      setTimeout(() => {
-        setDisabled(false);
-      }, 1000);
-    }, 200);
+    const updatedUser = await updateUser(userId, {
+      ...currentUser,
+      profileBackgroundImg: bgImgValue,
+      profileBackgroundBlur: blurValue,
+    });
+
+    devLog('updated user background details', updatedUser.user);
+
+    setButtonsDisabled(false);
   };
 
   return (
@@ -73,7 +65,7 @@ export default function ChangeBackground({ userBg, setUserBg, userId }) {
         <InputLabel>Change Profile Background</InputLabel>
         <Select
           value={userBg.img}
-          disabled={disabled}
+          disabled={buttonsDisabled}
           label="background image"
           onChange={handleChangeImage}>
           <MenuItem value="Summoners Rift">Summoners Rift</MenuItem>
@@ -85,7 +77,7 @@ export default function ChangeBackground({ userBg, setUserBg, userId }) {
       <FormControl style={{ marginLeft: '20px' }}>
         <InputLabel>Change Background Blur</InputLabel>
         <Select
-          disabled={disabled}
+          disabled={buttonsDisabled}
           value={userBg.blur}
           label="background image"
           onChange={handleChangeBlur}>
@@ -96,6 +88,20 @@ export default function ChangeBackground({ userBg, setUserBg, userId }) {
           <MenuItem value="20">Max</MenuItem>
         </Select>
       </FormControl>
+
+      <Button
+        style={{
+          height: '50px',
+          alignSelf: 'center',
+          marginLeft: '10px',
+          marginTop: '20px',
+        }}
+        startIcon={<SaveIcon />}
+        variant="contained"
+        disabled={buttonsDisabled}
+        onClick={onSaveBackground}>
+        Save
+      </Button>
     </FormGroup>
   );
 }
