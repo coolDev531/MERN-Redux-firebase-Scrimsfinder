cfc55835eef630f055f7aa028683bd0482494cc2
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-14 17:11:55 -0400

    replace Levenshtein with custom func

--- client/src/utils/levenshteinDistance.js
+++ client/src/utils/levenshteinDistance.js
@@ -1,23 +1,39 @@
+// for sorting users on search dropdown results.
 export const levenshteinDistance = (str1, str2) => {
-  const edits = [];
-  for (let i = 0; i < str2.length + 1; i++) {
-    const row = [];
+  const small = str1.length < str2.length ? str1 : str2;
+  const big = str1.length >= str2.length ? str1 : str2;
 
-    for (let j = 0; j < str1.length + 1; j++) {
-      row.push(j);
-    }
-    row[0] = i;
-    edits.push(row);
+  const evenEdits = [];
+  const oddEdits = new Array(small.length + 1);
+
+  for (let j = 0; j < small.length + 1; j++) {
+    evenEdits.push(j);
   }
-  for (let i = 1; i < str2.length + 1; i++) {
-    for (let j = 1; j < str1.length + 1; j++) {
-      if (str2[i - 1] === str1[j - 1]) {
-        edits[i][j] = edits[i - 1][j - 1];
+
+  for (let i = 1; i < big.length + 1; i++) {
+    let currentEdits, previousEdits;
+    if (i % 2 === 1) {
+      currentEdits = oddEdits;
+      previousEdits = evenEdits;
+    } else {
+      currentEdits = evenEdits;
+      previousEdits = oddEdits;
+    }
+
+    currentEdits[0] = i;
+
+    for (let j = 1; j < small.length + 1; j++) {
+      if (big[i - 1] === small[j - 1]) {
+        currentEdits[j] = previousEdits[j - 1];
       } else {
-        edits[i][j] =
-          1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1]);
+        currentEdits[j] =
+          1 +
+          Math.min(previousEdits[j - 1], previousEdits[j], currentEdits[j - 1]);
       }
     }
   }
-  return edits[str2.length][str1.length];
+
+  return big.length % 2 === 0
+    ? evenEdits[small.length]
+    : oddEdits[small.length];
 };
