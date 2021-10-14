bd3018d21f6d97ca6c2a99e983a8c3e55c266534
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-14 12:48:56 -0400

    update ScrimDetail error handling alerts

--- client/src/screens/ScrimDetail.jsx
+++ client/src/screens/ScrimDetail.jsx
@@ -7,27 +7,42 @@ import { Helmet } from 'react-helmet';
 import Loading from '../components/shared/Loading';
 import { PageContent } from '../components/shared/PageComponents';
 import { getScrimById } from '../services/scrims';
+import useAlerts from './../hooks/useAlerts';
 
 export default function ScrimDetail() {
   const { id } = useParams();
   const [scrim, setScrim] = useState(null);
   const { scrims } = useScrims();
   const history = useHistory();
+  const { setCurrentAlert } = useAlerts();
 
   useEffect(() => {
     const fetchScrimData = async () => {
       try {
         const scrimData = await getScrimById(id);
-        if (scrimData) {
+        if (scrimData?.createdBy) {
           setScrim(scrimData);
+        } else {
+          setCurrentAlert({
+            type: 'Error',
+            message: 'Scrim not found!, redirecting to home',
+          });
+          history.push('/scrims');
         }
-        // push to /scrims if scrim not found.
       } catch (error) {
+        console.log({ error });
+        setCurrentAlert({
+          type: 'Error',
+          message: 'Error finding scrim, redirecting to home',
+        });
+
         history.push('/scrims');
       }
     };
     fetchScrimData();
     // run this on mount and everytime scrims change.
+
+    // eslint-disable-next-line
   }, [id, scrims, history]);
 
   if (!scrim) return <Loading text="Loading Scrim Data..." />;
