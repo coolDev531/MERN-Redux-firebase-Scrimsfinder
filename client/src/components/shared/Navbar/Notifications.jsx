e3f23795239ae6c65b1f2c105c386f3c659ab588
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-14 20:49:50 -0400

    update notifications.jsx

--- client/src/components/shared/Navbar/Notifications.jsx
+++ client/src/components/shared/Navbar/Notifications.jsx
@@ -1,13 +1,18 @@
 import { useCallback } from 'react';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useSelector, useDispatch } from 'react-redux';
+
+// components
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '../Tooltip';
 import { Modal as NotificationsModal } from './../ModalComponents';
-import { useSelector, useDispatch } from 'react-redux';
 
 // icons
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import DeleteIcon from '@mui/icons-material/Delete';
+
+// services
 import { deleteOneUserNotification } from '../../../services/users';
 
 export default function Notifications() {
@@ -27,7 +32,7 @@ export default function Notifications() {
     dispatch({ type: 'general/closeNotifications' });
   }, [dispatch]);
 
-  if (!notifications.length) return null;
+  // if (!notifications.length) return null;
 
   return (
     <>
@@ -42,14 +47,23 @@ export default function Notifications() {
         open={notificationsOpen}
         onClose={closeNotifications}>
         <ul style={{ padding: 0, margin: 0 }}>
-          {notifications.map((notification) => (
-            <OneNotification
-              currentUserId={currentUser._id}
-              closeModal={closeNotifications}
-              key={notification._id}
-              notification={notification}
-            />
-          ))}
+          {notifications.length > 0 ? (
+            notifications.map((notification) => (
+              <OneNotification
+                currentUserId={currentUser._id}
+                closeModal={closeNotifications}
+                key={notification._id}
+                notification={notification}
+              />
+            ))
+          ) : (
+            <Typography
+              textAlign="center"
+              style={{ fontSize: '1rem' }}
+              variant="h6">
+              No notifications found
+            </Typography>
+          )}
         </ul>
       </NotificationsModal>
     </>
@@ -71,6 +85,7 @@ const OneNotification = ({ notification, closeModal, currentUserId }) => {
       payload: { notifications: newNotificationsState },
     });
   };
+
   return (
     <li
       style={{
@@ -79,7 +94,16 @@ const OneNotification = ({ notification, closeModal, currentUserId }) => {
         justifyContent: 'space-between',
         alignItems: 'center',
       }}>
-      <div style={{ border: '1px solid red' }}>{notification.message}</div>
+      <div
+        style={{ border: '1px solid red' }}
+        onClick={() => {
+          if (!notification?._relatedUser) return;
+          console.log(notification?._relatedUser);
+          closeModal();
+          // openFriendRequestsModal();
+        }}>
+        {notification.message}
+      </div>
 
       <div>
         <Tooltip title="Delete notification">
