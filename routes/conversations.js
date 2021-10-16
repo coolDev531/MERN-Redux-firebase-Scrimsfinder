6848aec535bae0983221e801b6e938bd63d8932d
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-16 13:42:39 -0400

    update conversations

--- routes/conversations.js
+++ routes/conversations.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const controllers = require('../controllers/conversations');
 
-// create  conv
+// create one conversation
 router.post('/conversations', controllers.postConversation);
 
-// get conv many of user by id.
+// get many conversations of user by id.
 router.get('/conversations/:userId', controllers.getUserConversations);
 
 module.exports = router;
