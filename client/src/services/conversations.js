918f0afe33e5ff99599ce936511f84bed5f2f007
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-16 15:27:53 -0400

    create messages services

--- client/src/services/conversations.js
+++ client/src/services/conversations.js
@@ -2,7 +2,7 @@ import api from './apiConfig';
 
 export const getUserConversations = async (userId) => {
   try {
-    const response = await api.get(`conversations/${userId}`);
+    const response = await api.get(`/conversations/${userId}`);
     return response.data;
   } catch (error) {
     throw error;
---
+++ client/src/services/messages.js
@@ -0,0 +1,27 @@
+import api from './apiConfig';
+
+/**
+ * @method postNewMessage
+ * @param {String} senderId
+ * @param {String} conversationId
+ * @returns {<{text: string, _conversation: string, _sender: string}>}
+ */
+export const postNewMessage = async ({ conversationId, senderId }) => {
+  try {
+    const response = await api.post(`/messages`, {
+      conversationId,
+      senderId,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+export const getConversationMessages = async (conversationId) => {
+  try {
+    const response = await api.get(`/messages/${conversationId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
