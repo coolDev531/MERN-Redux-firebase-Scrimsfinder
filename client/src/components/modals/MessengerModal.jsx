6acf9947d6c7a408f8915cd13d642ad09c123b4c
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-16 18:00:25 -0400

    update MessengerModal

--- client/src/components/modals/MessengerModal.jsx
+++ client/src/components/modals/MessengerModal.jsx
@@ -9,19 +9,27 @@ import {
 } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import useUsers from './../../hooks/useUsers';
+import useAuth from './../../hooks/useAuth';
+import useAlerts from './../../hooks/useAlerts';
 
 // components
 import Typography from '@mui/material/Typography';
-
+import OutlinedInput from '@mui/material/OutlinedInput';
 import { Modal } from './../shared/ModalComponents';
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
-
-import useAuth from './../../hooks/useAuth';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import LinearProgress from '@mui/material/LinearProgress';
 
 // services and utils
 import { getConversationMessages } from '../../services/messages.services';
 import makeStyles from '@mui/styles/makeStyles';
+
+// icons
+
+import CreateIcon from '@mui/icons-material/Create';
+import Tooltip from './../shared/Tooltip';
 
 export default function MessengerModal() {
   const [view, setView] = useState('all-conversations'); // all-conversations, chat-room
@@ -30,6 +38,11 @@ export default function MessengerModal() {
   const dispatch = useDispatch();
   const [{ conversations }, { messengerOpen }] = useSelector(
     ({ messenger, general }) => [messenger, general]
+  );
+
+  const modalTitle = useMemo(
+    () => (view === 'chat-room' ? 'Messenger (Chat Room)' : 'Messenger'),
+    [view]
   );
 
   const closeMessenger = useCallback(() => {
@@ -51,7 +64,7 @@ export default function MessengerModal() {
   }, []);
 
   return (
-    <Modal title="Messenger" open={messengerOpen} onClose={closeMessenger}>
+    <Modal title={modalTitle} open={messengerOpen} onClose={closeMessenger}>
       <button onClick={() => changeToView('all-conversations')}>
         all convos
       </button>
@@ -86,6 +99,9 @@ const ChatRoom = ({ conversation }) => {
   const { currentUser } = useAuth();
 
   const [messages, setMessages] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  const setCurrentAlert = useAlerts();
 
   const members = useMemo(() => {
     const currentUserIndex = conversation.members.findIndex(
@@ -111,15 +127,47 @@ const ChatRoom = ({ conversation }) => {
           );
         })
       );
+
+      setLoaded(true);
     };
 
     fetchMessages();
 
     // reset on component unmount
     return () => {
+      setLoaded(false);
       setMessages([]);
     };
   }, [conversation._id]);
+
+  const handleSubmitMessage = useCallback(
+    (msgText) => {
+      if (!msgText) {
+        setCurrentAlert({
+          type: 'Error',
+          message: 'cannot send message, input is empty!',
+        });
+      }
+
+      const newMessage = {
+        text: msgText,
+        _sender: currentUser,
+      };
+
+      return setMessages((prevState) => [...prevState, newMessage]);
+    },
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentUser?._id]
+  );
+
+  if (!loaded) {
+    return (
+      <div>
+        <LinearProgress sx={{ width: '100%' }} />
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -131,6 +179,8 @@ const ChatRoom = ({ conversation }) => {
           userName={message._sender.name}
         />
       ))}
+
+      <ChatInput onSubmit={handleSubmitMessage} />
     </div>
   );
 };
@@ -164,5 +214,31 @@ const ChatBubble = ({ isCurrentUser, messageText, userName }) => {
         <div className={classes.button}>{messageText}</div>
       </div>
     </div>
+  );
+};
+
+const ChatInput = ({ onSubmit }) => {
+  const [newMessage, setNewMessage] = useState('');
+
+  return (
+    <OutlinedInput
+      multiline
+      rows={2}
+      minRows={2}
+      maxRows={4}
+      sx={{ marginTop: 4, width: '40ch' }} // this width also expands the width of the modal (as wanted tbh)
+      placeholder="new message"
+      value={newMessage}
+      onChange={(e) => setNewMessage(e.target.value)}
+      endAdornment={
+        <InputAdornment position="end">
+          <Tooltip title="Send message">
+            <IconButton onClick={() => onSubmit(newMessage)}>
+              <CreateIcon />
+            </IconButton>
+          </Tooltip>
+        </InputAdornment>
+      }
+    />
   );
 };
