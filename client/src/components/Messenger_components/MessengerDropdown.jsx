060b9fa728122ec4596778796cea67191987dc64
HaradaYasuo <academic.dev0531@gmail.com>
2021-10-17 16:11:53 -0400

    update NewConversationFriends create a new Convo modal

--- client/src/components/Messenger_components/MessengerDropdown.jsx
+++ client/src/components/Messenger_components/MessengerDropdown.jsx
@@ -10,17 +10,23 @@ import Popper from '@mui/material/Popper';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '../shared/Tooltip';
 import UserConversations from './UserConversations';
 import NewConversationFriends from './NewConversationFriends';
 
 // utils
 import { KEYCODES } from '../../utils/keycodes';
 
+// icons
+import CreateIcon from '@mui/icons-material/Create';
+import ForumIcon from '@mui/icons-material/Forum';
+
 export default function MessengerDropdown({ open, setOpen, anchorRef }) {
   const { conversations } = useSelector(({ messenger }) => messenger);
 
   const [view, setView] = useState(() => {
-    return conversations.length > 0 ? 'existing' : 'new';
+    return conversations.length > 0 ? 'existing' : 'create-new';
   });
 
   const handleClose = useCallback(() => {
@@ -66,47 +72,67 @@ export default function MessengerDropdown({ open, setOpen, anchorRef }) {
                   item
                   container
                   alignItems="center"
+                  direction="row"
                   justifyContent="space-between">
-                  <Grid item xs={12}>
-                    {/* MESSENGER DROPDOWN TITLE */}
-                    <Typography
-                      sx={{
-                        marginLeft: '20px',
-                        marginTop: '20px',
-                        marginBottom: '10px',
-                      }}
-                      component="h1"
-                      gutterBottom
-                      variant="h6">
-                      Messenger
-                    </Typography>
+                  {/* MESSENGER DROPDOWN TITLE */}
+                  <Typography
+                    sx={{
+                      marginLeft: '20px',
+                      marginTop: '20px',
+                      marginBottom: '10px',
+                    }}
+                    component="h1"
+                    gutterBottom
+                    variant="h6">
+                    Messenger
+                  </Typography>
 
-                    <Divider />
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    {/* MESSENGER DROPDOWN SUBTITLE */}
-                    <Typography
-                      variant="body2"
-                      gutterBottom
-                      sx={{
-                        marginLeft: '20px',
-                        marginTop: '10px',
-                        marginBottom: '10px',
-                      }}>
-                      {view === 'existing'
-                        ? 'Existing conversations'
-                        : 'Start a new conversation'}
-                    </Typography>
-                    <Divider />
-
-                    {/* MESSENGER DROPDOWN CONTENT */}
-                    {view === 'existing' ? (
-                      <UserConversations closeMenu={handleClose} /> // close menu will close the drop down after opening a chat room in that component
+                  <Grid
+                    item
+                    style={{ marginRight: '-10px' }}
+                    container
+                    alignItems="center"
+                    xs={3}>
+                    {view === 'create-new' ? (
+                      <Tooltip title="View existing conversations">
+                        <IconButton onClick={() => setView('existing')}>
+                          <ForumIcon />
+                        </IconButton>
+                      </Tooltip>
                     ) : (
-                      <NewConversationFriends />
+                      <Tooltip title="Start a new conversation">
+                        <IconButton onClick={() => setView('create-new')}>
+                          <CreateIcon />
+                        </IconButton>
+                      </Tooltip>
                     )}
                   </Grid>
+
+                  <Divider sx={{ width: '100%' }} />
+                </Grid>
+
+                <Grid item xs={12}>
+                  {/* MESSENGER DROPDOWN SUBTITLE */}
+                  <Typography
+                    variant="body2"
+                    gutterBottom
+                    sx={{
+                      marginLeft: '20px',
+                      marginTop: '10px',
+                      marginBottom: '10px',
+                    }}>
+                    {view === 'existing'
+                      ? 'Existing conversations'
+                      : 'Start a new conversation'}
+                  </Typography>
+                  <Divider />
+
+                  {/* MESSENGER DROPDOWN CONTENT */}
+                  {view === 'existing' ? (
+                    <UserConversations closeMenu={handleClose} /> // close menu will close the drop down after opening a chat room in that component
+                  ) : (
+                    <NewConversationFriends />
+                  )}
                 </Grid>
               </Grid>
             </ClickAwayListener>
--- client/src/components/Messenger_components/NewConversationFriends.jsx
+++ client/src/components/Messenger_components/NewConversationFriends.jsx
@@ -1,5 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { Fragment, useCallback, useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import useAuth from './../../hooks/useAuth';
+import useAlerts from './../../hooks/useAlerts';
 import useUsers from './../../hooks/useUsers';
 
 // components
@@ -7,23 +9,32 @@ import Divider from '@mui/material/Divider';
 import Tooltip from '../shared/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import { Modal } from '../shared/ModalComponents';
 
 // utils
 import makeStyles from '@mui/styles/makeStyles';
 import { getRankImage } from '../../utils/getRankImage';
-import { useHistory } from 'react-router-dom';
+
+// icons
+import CreateIcon from '@mui/icons-material/Create';
 
 // a list of friends that the user doesn't have a conversation with yet.
 const NewConversationFriends = () => {
   const classes = useStyles();
   const { allUsers } = useUsers();
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
   const { onlineFriends, conversations } = useSelector(
     ({ messenger }) => messenger
   );
 
   const { currentUser } = useAuth();
-  const history = useHistory();
 
   const getFriendUserId = (arr) => {
     let friend = arr.find(({ _id }) => _id !== currentUser._id);
@@ -48,32 +59,107 @@ const NewConversationFriends = () => {
         if (inConversation) return null;
 
         return (
-          <MenuItem
-            onClick={() => {
-              history.push(
-                `/users/${friendUser.name}?region${friendUser.region}`
-              );
-            }}>
-            <Tooltip title="Move to conversation" key={friendUser._id}>
-              <div className={classes.user}>
-                <div
-                  // add this bool to user (use socket?) if onlien green, else red
-                  style={{ backgroundColor: isOnline ? '#AAFF00' : '#EE4B2B' }}
-                  className={classes.isOnlineCircle}></div>
-                <img
-                  src={getRankImage(friendUser)}
-                  alt={friendUser.rank}
-                  width="20px"
-                  className={classes.userRank}
-                />
-                {friendUser.name}
-                {idx !== arr.length - 1 ? <Divider /> : null}
-              </div>
-            </Tooltip>
-          </MenuItem>
+          <Fragment key={friendUser._id}>
+            <MenuItem onClick={() => setIsCreateModalOpen(friendUser._id)}>
+              <Tooltip title="Start a new conversation">
+                <div className={classes.user}>
+                  <div
+                    // add this bool to user (use socket?) if onlien green, else red
+                    style={{
+                      backgroundColor: isOnline ? '#AAFF00' : '#EE4B2B',
+                    }}
+                    className={classes.isOnlineCircle}></div>
+                  <img
+                    src={getRankImage(friendUser)}
+                    alt={friendUser.rank}
+                    width="20px"
+                    className={classes.userRank}
+                  />
+                  {friendUser.name}
+                  {idx !== arr.length - 1 ? <Divider /> : null}
+                </div>
+              </Tooltip>
+            </MenuItem>
+            <NewConversationModal
+              open={isCreateModalOpen === friendUser?._id}
+              setOpen={setIsCreateModalOpen}
+              receiverUser={friendUser}
+              currentUser={currentUser}
+            />
+          </Fragment>
         );
       })}
     </MenuList>
+  );
+};
+
+const NewConversationModal = ({ receiverUser, currentUser, open, setOpen }) => {
+  const [newMessage, setNewMessage] = useState('');
+  const { setCurrentAlert } = useAlerts();
+
+  const onSubmit = useCallback(async () => {
+    // send new conversation to api.
+    // close the new conversation modal
+    // open the chat room modal
+    if (!newMessage) {
+      setCurrentAlert({
+        type: 'Error',
+        message: 'cannot start a new conversation: message not provided!',
+      });
+    }
+
+    try {
+      setOpen(false);
+    } catch (error) {
+      console.error(error);
+      setCurrentAlert({
+        type: 'Error',
+        message: 'error starting a new conversation',
+      });
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newMessage]);
+
+  useEffect(() => {
+    if (!open) {
+      console.log('test');
+      setNewMessage('');
+    }
+  }, [open]);
+
+  if (!open) return null;
+
+  return (
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      title={`Start a new conversation`}>
+      <Grid item container direction="column">
+        <Typography variant="body2">
+          Start a new conversation with with {receiverUser.name}
+        </Typography>
+        <OutlinedInput
+          multiline
+          className="_draggable__input"
+          minRows={2}
+          maxRows={4}
+          sx={{ marginTop: 4, width: '98%' }} // this width also expands the width of the modal (as wanted tbh)
+          placeholder="new message"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          endAdornment={
+            <InputAdornment position="end">
+              <Tooltip title="Start the conversation">
+                <IconButton onClick={() => onSubmit(newMessage)}>
+                  <CreateIcon />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>
+          }
+        />
+      </Grid>
+    </Modal>
   );
 };
 
