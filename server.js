8baa63cd2659967981c0b9250237b371ca6d2cfb
HaradaYasuo <academic.dev0531@gmail.com>
2021-11-20 15:35:21 -0500

    update server.js for main

--- server.js
+++ server.js
@@ -17,17 +17,14 @@ const notificationRoutes = require('./routes/notification.routes');
 function createServer() {
   const app = express();
 
-  const allowedOrigins = [
-    'http://localhost:3001',
-    'https://lol-scrims-finder.netlify.app',
-  ];
+  const allowedOrigins = ['https://lol-scrims-finder.netlify.app'];
 
   const corsOptions = {
     origin: function (origin, callback) {
       // allow requests with no origin
       // (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
+      if (!allowedOrigins.includes(origin)) {
         const msg =
           'The CORS policy for this site does not ' +
           'allow access from the specified Origin.';
