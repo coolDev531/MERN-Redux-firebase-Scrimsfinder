5c5a526bda85d028cbe0cdfcce14575e6a564303
HaradaYasuo <academic.dev0531@gmail.com>
2022-02-08 20:40:01 -0500

    words api fix

--- client/src/services/wordsApi.js
+++ client/src/services/wordsApi.js
@@ -2,17 +2,13 @@ import axios from 'axios';
 
 export const generateRandomLobbyName = async () => {
   try {
-    const promise1 = await axios.get(
-      'https://random-words-api.vercel.app/word'
-    );
-    const promise2 = await axios.get(
-      'https://random-words-api.vercel.app/word'
-    );
+    const promise1 = axios.get('https://random-word-api.herokuapp.com/word');
+    const promise2 = axios.get('https://random-word-api.herokuapp.com/word');
 
     const response = await Promise.all([promise1, promise2]);
 
     // this api is returning an object which contains an array but the array will always have 1 object, Idk I was scratching my head.
-    const [first, second] = response.map(({ data }) => data[0].word);
+    const [{ data: first }, { data: second }] = response;
 
     return `${first} ${second}`;
   } catch (error) {
