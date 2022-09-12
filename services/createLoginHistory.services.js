5f2b29fdb7d2cf3a03cce782b5028d31d719ddf6
HaradaYasuo <academic.dev0531@gmail.com>
2022-09-12 14:15:04 -0400

    get rid of this for now

--- services/createLoginHistory.services.js
+++ services/createLoginHistory.services.js
@@ -1,34 +1,35 @@
-const { parseIp } = require('../utils/ip');
-const axios = require('axios');
-const LoginInfo = require('../models/login-info.model');
+// const { parseIp } = require('../utils/ip');
+// const axios = require('axios');
+// const LoginInfo = require('../models/login-info.model');
 require('dotenv').config();
 
 const createLoginHistory = async (req, user) => {
   try {
-    const ip = parseIp(req);
+    return true;
+    // const ip = parseIp(req);
 
-    const { data: ipData } = await axios.get(
-      `https://api.ipdata.co/${ip}/?api-key=${process.env.IPDATA_API_KEY}`
-    );
+    // const { data: ipData } = await axios.get(
+    //   `https://api.ipdata.co/${ip}/?api-key=${process.env.IPDATA_API_KEY}`
+    // );
 
-    if (ipData) {
-      const loginInfo = new LoginInfo({
-        _user: user._id,
-        ...ipData,
-      });
+    // if (ipData) {
+    //   const loginInfo = new LoginInfo({
+    //     _user: user._id,
+    //     ...ipData,
+    //   });
 
-      if (!user.loginHistory) {
-        user.loginHistory = [loginInfo];
-      } else {
-        user.loginHistory.push(loginInfo);
-      }
+    //   if (!user.loginHistory) {
+    //     user.loginHistory = [loginInfo];
+    //   } else {
+    //     user.loginHistory.push(loginInfo);
+    //   }
 
-      await loginInfo.save();
+    //   await loginInfo.save();
 
-      return true;
-    }
+    //   return true;
+    // }
 
-    return false;
+    // return false;
   } catch (error) {
     throw error;
   }
