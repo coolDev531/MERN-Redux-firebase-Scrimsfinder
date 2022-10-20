8d81ebeaed0955eace3759aba04ee99fdd924e4c
HaradaYasuo <academic.dev0531@gmail.com>
2022-10-20 10:24:00 -0400

    add fixed footer

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,20 +1,32 @@
 // components
 import Grid from '@mui/material/Grid';
 import { InnerColumn } from './PageComponents';
+import Sparkles from './effects/Sparkles';
 
 // utils
 import { styled } from '@mui/system';
+import { withRouter } from 'react-router-dom';
 
 const StyledFooter = styled('footer')({
   backgroundColor: 'rgba(18,25,35,.65)',
   boxShadow: '1px 2px 4px 1px#fff',
   scrollMarginTop: '2em',
+  overflow: 'hidden',
+
+  // fixed footer.
+  position: 'fixed',
+  bottom: '0',
+  width: '100%',
 });
 
 const currentYear = new Date().getFullYear();
 
-const Footer = () => (
+const blacklist = ['/scrims', '/'];
+
+const Footer = ({ location }) => (
   <>
+    {!blacklist.includes(location.pathname) && <div className="page-break" />}
+
     <div className="footer-spacer" />
     <StyledFooter className="page-section site-footer">
       <InnerColumn>
@@ -34,17 +46,19 @@ const Footer = () => (
             Source
           </a> */}
 
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="link"
-            href="https://www.danielmichaelmusic.com">
-            Support my music
-          </a>
+          <Sparkles>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+              href="https://www.danielmichaelmusic.com">
+              Support my music
+            </a>
+          </Sparkles>
         </Grid>
       </InnerColumn>
     </StyledFooter>
   </>
 );
 
-export default Footer;
+export default withRouter(Footer);
